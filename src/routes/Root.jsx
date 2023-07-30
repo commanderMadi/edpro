924784b3d1747553e6ee34bfd538a6bda495bbbc
Antoine Taillard <an.taillard@gmail.com>
2023-07-30 10:04:28 +0200

    Setup the project

---
+++ src/routes/Root.jsx
@@ -0,0 +1,12 @@
+function Root() {
+
+  return (
+    <>
+    <div className="flex justify-center">
+      <h1 className="font-bold">EdPro - Let's start building</h1>
+    </div>
+    </>
+  )
+}
+
+export default Root
