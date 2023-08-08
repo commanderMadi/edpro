40b146186dc0bc128baf9caab4f767b4780eca1f
Antoine Taillard <an.taillard@gmail.com>
2023-08-08 13:26:37 +0200

    Added course view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 function Navbar() {
 
   return (
-    <div className="flex justify-between px-8 py-5 bg-accent text-white">
-      <div className="text-lg font-bold">EdPro</div>
+    <div className="flex justify-between px-8 py-5 bg-accent text-white text-lg">
+      <div className="text-2xl font-bold">EdPro</div>
       <nav className="flex space-x-8 mr-5">
         <Link to={`courses`}>Catalog</Link>
         <Link to={`signin`}>Sign In</Link>
--- src/routes/Course.jsx
+++ src/routes/Course.jsx
@@ -1,5 +1,9 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { useDocument } from "../hooks/useDocument"
+
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
 
 function Course() {
   const { id } = useParams()
@@ -14,7 +18,31 @@ function Course() {
 
   return (
     <div>
-      <h1>{course.title}</h1>
+      <img src={course.imgURL} className="object-cover w-full h-80"/>
+      <div className="p-5">
+        <div className="flex">
+          <h1 className="text-2xl font-bold text-accent">{course.title}</h1>
+          <div className="px-4">
+            <Rating 
+              style={{ maxWidth: 180 }}
+              value={course.rating}
+              readOnly
+            />
+          </div>
+        </div>
+        <div className="text-slate-500">
+          <p>{course.months} months, at {course.weeklyHours} hours per week</p>
+          <p>{course.students} students enrolled</p>
+        </div>
+        <div className="py-5 mr-96">
+          <p className="text-2xl pb-4">What's in it for you?</p>
+          <p className="text-lg">{course.description}</p>
+        </div>
+        <div className="flex justify-end">
+          <Link to='/signin' className="btn bg-gray-100 text-accent mr-4">Sign In To Enroll</Link>
+          <Link to='/courses' className="btn bg-red-700 text-white ">Go Back</Link>
+        </div>
+      </div>
     </div>
   )
 }
