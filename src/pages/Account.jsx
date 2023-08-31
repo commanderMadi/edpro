79aff9cf524db3e1ac1aa87318d9bbbc6d8c2dd5
Ahmed Magdy <madimagdy@gmail.com>
2023-08-31 21:41:06 +0900

    Initial Account page logic

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,10 +1,46 @@
-function Account() {
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { db } from '../firebase/config';
+import { collection } from 'firebase/firestore';
+import { getDocs } from 'firebase/firestore';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useDocument } from '../hooks/useDocument';
+import { useCollection } from '../hooks/useCollection';
 
-  return (
-    <div>
-      Account
-    </div>
-  )
-}
+const Account = () => {
+    const [name, setName] = useState(null);
+    const { user } = useAuthContext();
+    const { document: users, error } = useDocument('users', user.uid);
+    const [fullName, setFullName] = useState(null);
 
-export default Account
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                if (users) {
+                    setFullName(users.fullName);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchUser();
+    }, [users]);
+
+    return (
+        <>
+            {!users && (
+                <div>
+                    <h1>Loading.. Please wait!</h1>
+                </div>
+            )}
+
+            {users && (
+                <div>
+                    <h1>Hello there, {fullName}</h1>
+                </div>
+            )}
+        </>
+    );
+};
+export default Account;
