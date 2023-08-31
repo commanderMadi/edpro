cd04926d59b7ecdd7e3ac7656b87edaf8b288ac3
Ahmed Magdy <madimagdy@gmail.com>
2023-08-31 21:40:49 +0900

    Fix formatting

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,56 +1,58 @@
-import { Link, useParams } from "react-router-dom"
-import { Rating } from '@smastrom/react-rating'
-import '@smastrom/react-rating/style.css'
+import { Link, useParams } from 'react-router-dom';
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css';
 
-import { useAuthContext } from "../hooks/useAuthContext"
-import { useDocument } from "../hooks/useDocument"
-import EnrollButton from "../components/EnrollButton"
-
-
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useDocument } from '../hooks/useDocument';
+import EnrollButton from '../components/EnrollButton';
 
 function Course() {
-  const { id } = useParams()
-  const { user } = useAuthContext()
-  const { document: course, error } = useDocument('courses', id)
-  
-  if (error) {
-    return <div>{error}</div>
-  }
+    const { id } = useParams();
+    const { user } = useAuthContext();
+    const { document: course, error } = useDocument('courses', id);
 
-  if (!course) {
-    return <div>Loading...</div>
-  }
+    if (error) {
+        return <div>{error}</div>;
+    }
 
-  return (
-    <div>
-      <img src={course.imgURL} className="object-cover w-full h-80"/>
-      <div className="p-5">
-        <div className="flex">
-          <h1 className="text-2xl font-bold ">{course.title}</h1>
-          <div className="px-4">
-            <Rating 
-              style={{ maxWidth: 180 }}
-              value={course.rating}
-              readOnly
-            />
-          </div>
+    if (!course) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <img src={course.imgURL} className='object-cover w-full h-80' />
+            <div className='p-5'>
+                <div className='flex'>
+                    <h1 className='text-2xl font-bold '>{course.title}</h1>
+                    <div className='px-4'>
+                        <Rating style={{ maxWidth: 180 }} value={course.rating} readOnly />
+                    </div>
+                </div>
+                <div className='text-slate-500'>
+                    <p>
+                        {course.months} months, at {course.weeklyHours} hours per week
+                    </p>
+                    <p>{course.students} students enrolled</p>
+                </div>
+                <div className='py-5 mr-96'>
+                    <p className='text-2xl pb-4'>What's in it for you?</p>
+                    <p className='text-lg'>{course.description}</p>
+                </div>
+                <div className='flex justify-end space-x-4'>
+                    {user && <EnrollButton userID={user.uid} course={course} />}
+                    {!user && (
+                        <Link to='/login' className='btn-primary'>
+                            Login to Enroll
+                        </Link>
+                    )}
+                    <Link to='/courses' className='btn-secondary'>
+                        Go Back
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className="text-slate-500">
-          <p>{course.months} months, at {course.weeklyHours} hours per week</p>
-          <p>{course.students} students enrolled</p>
-        </div>
-        <div className="py-5 mr-96">
-          <p className="text-2xl pb-4">What's in it for you?</p>
-          <p className="text-lg">{course.description}</p>
-        </div>
-        <div className="flex justify-end space-x-4">
-          {user && <EnrollButton userID={user.uid} course={course} />}
-          {!user && <Link to='/login' className="btn-primary">Login to Enroll</Link>}
-          <Link to='/' className="btn-secondary">Go Back</Link>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Course
+export default Course;
