d0071777588400838fb56dfffada7182c1e91189
Ahmed Magdy <madimagdy@gmail.com>
2023-09-03 13:52:12 +0900

    add quicksand font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'accent': '#1a187a'
-      },
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            sans: ['Quicksand', 'ui-sans-serif', 'system-ui'],
+            serif: ['ui-serif', 'Georgia'],
+            mono: ['ui-monospace', 'SFMono-Regular'],
+            display: ['Oswald'],
+            body: ['"Open Sans"'],
+        },
+        extend: {
+            colors: {
+                accent: '#1a187a',
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
