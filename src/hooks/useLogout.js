5a1b06eedb3eba2c01c8742ae2024a5ccf9d3016
Ahmed Magdy <madimagdy@gmail.com>
2023-09-03 14:05:12 +0900

    redirect user to homepage on logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,32 +1,31 @@
-import { useState } from "react"
-import { auth } from "../firebase/config"
-import { useAuthContext } from "./useAuthContext"
-import { signOut } from "firebase/auth"
+import { useState } from 'react';
+import { auth } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
+import { signOut } from 'firebase/auth';
 
 export const useLogout = () => {
-  const [error, setError] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
+    const [error, setError] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const { dispatch } = useAuthContext();
 
-  const logout = async () => {
-    setError(null)
-    setIsPending(null)
+    const logout = async () => {
+        setError(null);
+        setIsPending(null);
 
-    try {
-      //logout the user
-      await signOut(auth)
+        try {
+            //logout the user
+            await signOut(auth);
 
-      //dispatch the action
-      dispatch({ type: 'LOGOUT' })
-      setIsPending(false)
+            //dispatch the action
+            dispatch({ type: 'LOGOUT' });
+            setIsPending(false);
+            window.location = '/';
+        } catch (err) {
+            console.log(err);
+            setError(err.message);
+            setIsPending(false);
+        }
+    };
 
-    }
-    catch(err) {
-      console.log(err)
-      setError(err.message)
-      setIsPending(false)
-    }
-  }
-  
-  return { error, isPending, logout }
-}
+    return { error, isPending, logout };
+};
