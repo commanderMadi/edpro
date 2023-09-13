5df1f13b468d07089d92220894d26c39d8b50511
Ahmed Magdy <madimagdy@gmail.com>
2023-09-13 23:20:28 +0900

    change color accent for forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,55 +1,63 @@
-import { useState } from "react"
-import { Navigate } from "react-router-dom"
-import { useLogin } from "../hooks/useLogin"
-import { useAuthContext } from "../hooks/useAuthContext"
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useLogin } from '../hooks/useLogin';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const { error, isPending, login } = useLogin()
-  const { user } = useAuthContext()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { error, isPending, login } = useLogin();
+    const { user } = useAuthContext();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    login(email, password)
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        login(email, password);
+    };
 
-  if (user) {
-    return <Navigate to='/my-courses/enrolled' replace/>
-  }
+    if (user) {
+        return <Navigate to='/my-courses/enrolled' replace />;
+    }
 
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <h2 className="text-xl text-accent font-medium my-12">Welcome back! Ready to resume your learning journey?</h2>
-      <form onSubmit={handleSubmit} className="bg-gray-50 p-6 pt-12">
-        <label className="labl-txt">
-          <span>Email</span>
-          <input
-            required
-            type='email'
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            className="txt-inpt"
-          />
-        </label>
-        <label className="labl-txt">
-          <span>Password</span>
-          <input
-            required
-            type='password'
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            className="txt-inpt"
-          />
-        </label>
-        <div className="flex justify-end mt-8">
-          {!isPending && <button className='btn-primary'>Login</button>}
-          {isPending && <button className='btn-primary' disabled>Loading</button>}
+    return (
+        <div className='flex flex-col  justify-center items-center'>
+            <h2 className='text-xl text-accent font-medium my-12'>
+                Welcome back! Ready to resume your learning journey?
+            </h2>
+            <form onSubmit={handleSubmit} className='bg-accent  p-6 pt-12'>
+                <label className='labl-txt'>
+                    <span className='text-white'>Email</span>
+                    <input
+                        required
+                        type='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        className='txt-inpt'
+                    />
+                </label>
+                <label className='labl-txt'>
+                    <span className='text-white'>Password</span>
+                    <input
+                        required
+                        type='password'
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        className='txt-inpt'
+                    />
+                </label>
+                <div className='flex justify-end mt-8'>
+                    {!isPending && (
+                        <button className='btn-primary !text-white'>Login</button>
+                    )}
+                    {isPending && (
+                        <button className='btn-primary' disabled>
+                            Loading
+                        </button>
+                    )}
+                </div>
+            </form>
+            {error && <div className='error'>{error}</div>}
         </div>
-      </form>
-      {error && <div className='error'>{error}</div>}
-    </div>
-  )
+    );
 }
-  
-export default Login
+
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,81 +1,90 @@
-import { useState } from 'react'
-import { Navigate } from "react-router-dom"
-import { useRegister } from '../hooks/useRegister'
-import { useAuthContext } from "../hooks/useAuthContext"
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useRegister } from '../hooks/useRegister';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Register() {
-  const [fullName, setFullName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const { error, isPending, register } = useRegister()
-  const { user } = useAuthContext()
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { error, isPending, register } = useRegister();
+    const { user } = useAuthContext();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    register(fullName, email, password)
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        register(fullName, email, password);
+    };
 
-  const checkPassword = (e) => {
-    e.preventDefault()
-    e.target.value == password ? 
-      e.target.setCustomValidity('') : e.target.setCustomValidity('Password do not match')
-  }
+    const checkPassword = (e) => {
+        e.preventDefault();
+        e.target.value == password
+            ? e.target.setCustomValidity('')
+            : e.target.setCustomValidity('Password do not match');
+    };
 
-  if (user) {
-    return <Navigate to='/my-courses/enrolled' replace/>
-  }
+    if (user) {
+        return <Navigate to='/my-courses/enrolled' replace />;
+    }
 
-  return (
-    <div className='flex flex-col justify-center items-center'>
-      <h2 className="text-xl text-accent font-medium my-12">Welcome to EdPro. Let the learning begin!</h2>
-      <form onSubmit={handleSubmit} className='bg-gray-50 p-6 pt-12'>
-        <label className='labl-txt'>
-          <span>Full Name</span>
-          <input
-            required
-            type='text'
-            onChange={(e) => setFullName(e.target.value)}
-            value={fullName}
-            className='txt-inpt'
-          />
-        </label>
-        <label className='labl-txt'>
-          <span>Email</span>
-          <input
-            required
-            type='email'
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            className='txt-inpt'
-          />
-        </label>
-        <label className='labl-txt'>
-          <span>Password</span>
-          <input
-            required
-            type='password'
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            className='txt-inpt'
-          />
-        </label>
-        <label className='labl-txt'>
-          <span>Confirm Password</span>
-          <input
-            required
-            type='password'
-            onChange={checkPassword}
-            className='txt-inpt'
-          />
-        </label>
-        <div className="flex justify-end mt-8">
-          {!isPending && <button className='btn-primary'>Sign up</button>}
-          {isPending && <button className='btn-primary' disabled>Loading</button>}
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <h2 className='text-xl text-accent font-medium my-12'>
+                Welcome to EdPro. Let the learning begin!
+            </h2>
+            <form onSubmit={handleSubmit} className='bg-accent p-6 pt-12'>
+                <label className='labl-txt'>
+                    <span className='text-white'>Full Name</span>
+                    <input
+                        required
+                        type='text'
+                        onChange={(e) => setFullName(e.target.value)}
+                        value={fullName}
+                        className='txt-inpt'
+                    />
+                </label>
+                <label className='labl-txt'>
+                    <span className='text-white'>Email</span>
+                    <input
+                        required
+                        type='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        className='txt-inpt'
+                    />
+                </label>
+                <label className='labl-txt'>
+                    <span className='text-white'>Password</span>
+                    <input
+                        required
+                        type='password'
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        className='txt-inpt'
+                    />
+                </label>
+                <label className='labl-txt'>
+                    <span className='text-white'>Confirm Password</span>
+                    <input
+                        required
+                        type='password'
+                        onChange={checkPassword}
+                        className='txt-inpt'
+                    />
+                </label>
+                <div className='flex justify-end mt-8'>
+                    {!isPending && (
+                        <button className='btn-primary !text-white'>Sign up</button>
+                    )}
+                    {isPending && (
+                        <button className='btn-primary' disabled>
+                            Loading
+                        </button>
+                    )}
+                </div>
+                {error && <div>{error}</div>}
+            </form>
         </div>
-        {error && <div>{error}</div>}
-      </form>
-    </div>
-  )
+    );
 }
 
-export default Register
+export default Register;
